139a1dbcf9d8bef1986bfe76c57ddec42363eaf2
Ciceroni916 <32909951+Ciceroni916@users.noreply.github.com>
2025-09-19 16:28:37 +0700

    final question mark

---
+++ Assets/Scripts/SO/soundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundManager", menuName = "ScriptableObjects/SO", order = 1)]
+public class soundManager : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
